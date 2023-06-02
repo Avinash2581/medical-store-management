750b8c496e666a98ef7fa6da2c142c0b49ff6785
Avinash2581 <sahuavinash91141@gmail.com>
2023-06-02 10:26:02 +0530

    first commit

--- src/main/java/com/kloc/test/repository/BIRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kloc.test.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kloc.test.entities.BillItems;
-
-public interface BIRepository extends JpaRepository<BillItems, Integer> {
-
-}
